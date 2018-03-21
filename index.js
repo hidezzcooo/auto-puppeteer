ff82a47b33cca9d8d9f0ea982d544c64eb1af811
范小六 <fanzhiqiang@autohome.com.cn>
2018-03-21 10:54:14 +0800

    add download chromium

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const puppeteer = require('puppeteer')
+const readline = require('readline')
 const fs = require('fs')
-const target = process.argv.splice(2)[0]
-const LOG_ARR = ['response.txt', 'failure.txt']
+const log_arr = ['response.txt', 'failure.txt']
+const revision = process.argv[3] || '533271'
 let deleteTxt = txtArr => txtArr.forEach(item => fs.exists(item, exists => exists && fs.unlink(item, err => err)))
 
 try {
   (async () => {
     // clean previously log
-    await deleteTxt(LOG_ARR)
+    await deleteTxt(log_arr)
 
     // create browser & set chromium app path
     // const browser = await puppeteer.launch({executablePath: '/Applications/Chromium.app/Contents/MacOS/Chromium'})
-    const browser = await puppeteer.launch()
+    const browserFetcher = puppeteer.createBrowserFetcher()
+    const revisionInfo = await browserFetcher.download(revision, (dB, tB) => {
+      readline.cursorTo(process.stdout, 0)
+      process.stdout.write(`  Downloading chromium(${revision}) ... ${((dB/tB) * 100).toFixed(1)}%`)
+    })
+    const browser = await puppeteer.launch({executablePath: revisionInfo.executablePath})
     const browser_version = await browser.version()
     const page = await browser.newPage()
     let response_log = ''
@@ -26,7 +32,7 @@ try {
     	if (responseType === 'script' || responseType === 'image' || responseType === 'stylesheet') {
       	if( response.status() >= 400 ){
   		    response_log += `-Url   : ${response.url()}\n-Status: ${response.status()}\n-Type  : ${responseType}\n\n\n`
-  		    fs.writeFile(LOG_ARR[0], response_log, err => err)
+  		    fs.writeFile(log_arr[0], response_log, err => err)
       	}
     	}
     })
@@ -34,16 +40,16 @@ try {
     // listener for requestfail
     page.on('requestfailed', request => {
     	failed_log += `-Url   : ${request.url()}\n-ERR   : ${request.failure().errorText}\n\n`
-    	fs.writeFile(LOG_ARR[1], failed_log, err => err)
+    	fs.writeFile(log_arr[1], failed_log, err => err)
     })
 
-    console.log(`... Checking : <${target}> ...`)
-    console.log(`... Browser version : ${browser_version}`)
-    await page.goto(target)
+    console.log(`\n  Checking : <${process.argv[2]}> ...`)
+    console.log(`  Browser version : ${browser_version}`)
+    await page.goto(process.argv[2])
     // after 5s timeout
     await page.waitFor(5000)
     browser.close()
-    console.log('... Done ...')
+    console.log('  Done ...')
   })()
 } catch (err) {
   console.log(err)
